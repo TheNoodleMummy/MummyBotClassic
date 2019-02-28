537fa879af736cd7fa4e40395eefc790e06e79a8
Peter <petervanroie@hotmail.com>
2019-02-28 19:45:12 +0100

    gitignore isnt helping :(

--- MummybotRewrite/Attributes/InjectAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Mummybot.Attributes
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
-    public class InjectAttribute : Attribute
-    {
-    }
-}
