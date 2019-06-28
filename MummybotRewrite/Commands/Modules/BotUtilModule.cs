e6e6c04e2daa32d6ddc54fdb93879cca0ea8bc83
Peter <petervanroie@hotmail.com>
2019-06-28 04:52:32 +0200

    added ping service and ping command

---
+++ MummybotRewrite/Commands/Modules/BotUtilModule.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Mummybot.Services;
+using Qmmands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mummybot.Commands.Modules
+{
+    public class BotUtilModule : MummyBase
+    {
+        public PingService PingService { get; set; }
+
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            var emb = new EmbedBuilder()
+                .AddField("My average gateway Ping in the last hour", PingService.HourPing.ToString("##.##"), true)
+                .AddField("My total average ping", PingService.TotalPing.ToString("##.##"), true);
+
+            var latency = Context.Client.Latency;
+            var s = Stopwatch.StartNew();
+
+            var m = await ReplyAsync($"heartbeat: {latency}ms, init: ---, rtt: ---", embed: emb);
+            var init = s.ElapsedMilliseconds;
+            await m.ModifyAsync(x =>
+            {
+                x.Content = $"heartbeat: {latency}ms, init: {init}ms, rtt: Calculating";
+                x.Embed = emb.Build();
+            });
+            s.Stop();
+            await m.ModifyAsync(x =>
+            {
+                x.Content = $"heartbeat: {latency}ms, init: {init}ms, rtt: {s.ElapsedMilliseconds}ms";
+                x.Embed = emb.Build();
+            });
+
+           
+        }
+    }
+}
---
+++ MummybotRewrite/Services/PingService.cs
@@ -0,0 +1,34 @@
+﻿using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mummybot.Services
+{
+    public class PingService
+    {
+
+        private Dictionary<DateTime, int> HourlyPings = new Dictionary<DateTime, int>();
+        private List<int> Pings = new List<int>();
+
+        public double HourPing => HourlyPings.Select(x=>x.Value).Average();
+        public double TotalPing => Pings.Average();
+
+        public PingService(DiscordSocketClient client)
+        {
+            client.LatencyUpdated += (_, newping) =>
+            {
+                Pings.Add(newping);
+                HourlyPings.Add(DateTime.UtcNow, newping);
+                var toremove = HourlyPings.Where(x => x.Key <= DateTime.UtcNow);
+                foreach (var item in toremove)
+                {
+                    HourlyPings.Remove(item.Key);
+                }
+                return Task.CompletedTask;
+            };
+        }
+    }
+}
