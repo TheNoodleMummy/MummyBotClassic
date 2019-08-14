6f8c3c51c47b0936d9a7e01f51e391ae4e675633
The Noodle Mummy <petervanroie@htmail.com>
2019-08-14 18:07:37 +0200

    fixed gitignore issue

---
+++ MummybotRewrite/Migrations/GuildStoreMigrations/20190814155808_temptest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mummybot.Migrations.GuildStoreMigrations
+{
+    public partial class temptest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "temp",
+                table: "VoiceMutedUser",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "temp",
+                table: "VoiceMutedUser");
+        }
+    }
+}
