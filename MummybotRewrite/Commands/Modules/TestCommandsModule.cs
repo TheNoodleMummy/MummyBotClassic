b1a1ec91f42fcce9b5d93d2885be4c2b009ecedf
The Noodle Mummy <petervanroie@hotmail.com>
2020-01-06 05:06:51 +0100

    added a stupid check id command, it lets you check when you created a thing in discord by its id and shows also all other details hidden in the ids

--- MummybotRewrite/Commands/Modules/TestCommandsModule.cs
+++ MummybotRewrite/Commands/Modules/TestCommandsModule.cs
@@ -14,7 +14,17 @@ namespace Mummybot.Commands.Modules
 {
 	public class TestCommandModule : MummyModule
     {
+        [Command("inspect")]
+        public async Task inspectid(ulong id)
+        {
+            var time = DateTimeOffset.FromUnixTimeMilliseconds((long)(id >> 22)+ 1420070400000);
+            var workerid = (id & 0x3E0000) >> 17;
+            var processid = (id & 0x1F000) >> 12;
+            var increment = id & 0xFFF;
 
+            await ReplyAsync($"{id} was created on: {time}, with workerid: {workerid}, processid: {processid}, and increment: {increment}");
+
+        }
         
     }
 }
