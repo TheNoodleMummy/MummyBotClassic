6c01313efa73a853bf45ba8da2a32b93eda3b8b4
The Noodle Mummy <petervanroie@hotmail.com>
2020-02-29 19:40:18 +0100

    /shurg forgot to build migrtions in the last one...

--- MummybotRewrite/Migrations/GuildStoreMigrations/20200229183953_playlistwhitelist.Designer.cs
+++ MummybotRewrite/Migrations/GuildStoreMigrations/20200229183953_playlistwhitelist.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Mummybot.Database;
 
 namespace Mummybot.Migrations.GuildStoreMigrations
 {
     [DbContext(typeof(GuildStore))]
-    partial class GuildStoreModelSnapshot : ModelSnapshot
+    [Migration("20200229183953_playlistwhitelist")]
+    partial class playlistwhitelist
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
